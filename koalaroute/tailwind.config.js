6c0e99fc4f30f6e16a5396567cab4e527e188ab3
Birajsitaula <birajsitaula7@gmail.com>
2025-08-19 15:33:16 +0545

    last commit

---
+++ koalaroute/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // All pages in app folder
+    "./src/components/**/*.{js,ts,jsx,tsx}", // All components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
